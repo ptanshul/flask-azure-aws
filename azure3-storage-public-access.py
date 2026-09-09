5229a5ecbb73afe866925df64c7f701198d9e287
ptanshul <anshul.tiwari@rediffmail.com>
2026-09-09 20:38:44 +0530

    storeage-public-access-commit2

--- azure3-storage-public-access.py
+++ azure3-storage-public-access.py
@@ -20,5 +20,5 @@ for account in client.storage_accounts.list():
   print(f"Min TLS Version:  {account.minimum_tls_version}")
   print(f"Public Access:    {account.allow_blob_public_access}")
   print(f"Tags:             {account.tags}")
-  print(f"ID:             {account.id}")
+  print(f"public access True or False:             {account.allow_blob_public_access}")
   print("=" * 60)
