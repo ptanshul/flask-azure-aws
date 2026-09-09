1d7bb5cd7bd9cdd543f4bcb91a6674799d3648ca
ptanshul <anshul.tiwari@rediffmail.com>
2026-09-09 19:17:08 +0530

    commit4

--- split1.py
+++ split1.py
@@ -3,3 +3,17 @@ str="apple/banana/cherry"
 fruits = str.split("/")[1]
 print(fruits)
 print(str)
+
+raw_id = "/subscriptions/1111-aaaa/resourceGroups/prod-network-rg/providers/Microsoft.Storage/storageAccounts/appdata"
+
+# Full split
+pieces = raw_id.split("/")
+
+# Extracting specific parts:
+subscription = raw_id.split("/")[2]  # "1111-aaaa"
+resource_group = raw_id.split("/")[4]  # "prod-network-rg"
+service_type = raw_id.split("/")[7]  # "storageAccounts"
+account_name = raw_id.split("/")[8]  # "appdata"
+
+print(f"Resource Group: {resource_group}")
+# Output: Resource Group: prod-network-rg
