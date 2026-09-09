620812001848b7db0933c36797141db5c7fdfb1c
ptanshul <anshul.tiwari@rediffmail.com>
2026-09-09 18:42:29 +0530

    storage1-commit1

---
+++ app-storage1.py
@@ -0,0 +1,14 @@
+import os
+from azure.identity import DefaultAzureCredential
+from azure.mgmt.storage import StorageManagementClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = StorageManagementClient(
+    DefaultAzureCredential(), os.getenv("AZURE_SUBSCRIPTION_ID")
+)
+
+for account in client.storage_accounts.list():
+  rg = account.id.split("/")[4]
+  print(f"Account: {account.name} | RG: {rg}")
