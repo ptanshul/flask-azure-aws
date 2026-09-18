4582ac5c01e69ee8008b1922128d0c06df11e7e0
ptanshul <anshul.tiwari@rediffmail.com>
2026-09-18 11:07:01 +0530

    fastapi-commit5

--- fastapi2.py
+++ fastapi2.py
@@ -15,8 +15,10 @@ def add(a: int, b: int):
 @app.get("/subtract")
 def subtract(a: int, b: int):
     return {"result": a - b}
+@app.get("/multiply")
 def multiply(a: int, b: int):
     return {"result": a * b}
+@app.get("/divide")
 def divide(a: int, b: int):
     if b == 0:
         return {"error": "Division by zero is not allowed."}
